f5f28e260893b890bc22eca95d58f21a4d53f201
Bajarang Jadhav <bajarangjadhav2019@gmail.com>
2020-09-22 21:25:29 +0530

    Updates

---
+++ precache-manifest.516e853a00bc2693a2f878d11a2190fe.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bbb29f8625cb5b0e96431ad430daa60",
+    "url": "/react-bootstrap-complete-app/index.html"
+  },
+  {
+    "revision": "de3fe0d9adbc4f456d44",
+    "url": "/react-bootstrap-complete-app/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "3c6de4ee2446c79bd87d",
+    "url": "/react-bootstrap-complete-app/static/css/main.d7084c92.chunk.css"
+  },
+  {
+    "revision": "de3fe0d9adbc4f456d44",
+    "url": "/react-bootstrap-complete-app/static/js/2.e9736cb3.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/react-bootstrap-complete-app/static/js/2.e9736cb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c6de4ee2446c79bd87d",
+    "url": "/react-bootstrap-complete-app/static/js/main.332615f7.chunk.js"
+  },
+  {
+    "revision": "50b3907488350861fc22",
+    "url": "/react-bootstrap-complete-app/static/js/runtime-main.c9b09e1c.js"
+  },
+  {
+    "revision": "488a1443af9e39d0a449cdaf3f8e7d97",
+    "url": "/react-bootstrap-complete-app/static/media/android.488a1443.svg"
+  },
+  {
+    "revision": "b3727bbd3b8a1c848600ad1d5f4ee9d9",
+    "url": "/react-bootstrap-complete-app/static/media/flag.b3727bbd.svg"
+  },
+  {
+    "revision": "0716aba552638647f2b204c07f736b1c",
+    "url": "/react-bootstrap-complete-app/static/media/global-network.0716aba5.svg"
+  },
+  {
+    "revision": "e9a248b4b6e701c247644937e7c533ac",
+    "url": "/react-bootstrap-complete-app/static/media/mobile-app.e9a248b4.svg"
+  },
+  {
+    "revision": "71a7fc7d155fc5cd972f856ffdb4a5e4",
+    "url": "/react-bootstrap-complete-app/static/media/network.71a7fc7d.svg"
+  },
+  {
+    "revision": "e2f7de8b1d662f52c7409059ac845efb",
+    "url": "/react-bootstrap-complete-app/static/media/python.e2f7de8b.svg"
+  },
+  {
+    "revision": "eab76719fd611cab5eecb1eb1427675a",
+    "url": "/react-bootstrap-complete-app/static/media/react.eab76719.svg"
+  },
+  {
+    "revision": "dde8bff33933455020fe4a37a61486db",
+    "url": "/react-bootstrap-complete-app/static/media/search.dde8bff3.svg"
+  },
+  {
+    "revision": "66fe63746b1e8e6f468052d8fb01d7fd",
+    "url": "/react-bootstrap-complete-app/static/media/youtube.66fe6374.svg"
+  }
+]);
